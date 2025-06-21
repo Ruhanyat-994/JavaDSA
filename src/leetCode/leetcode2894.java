1e0ed7fe2f57c1defdfb6cff324de03367f4ffda
Ruhanyat-994 <alruhanyat994@gmail.com>
2025-06-22 00:47:54 +0600

    Added Leet Code Question Seris on the DSA Repo

---
+++ src/leetCode/leetcode2894.java
@@ -0,0 +1,15 @@
+package leetCode;
+
+public class leetcode2894 {
+
+        public int differenceOfSums(int n, int m) {
+
+            int x = n/m;
+            int num2 = m*x*(x+1)/2;
+            int num1 = n*(n+1)/2 - num2;
+
+            return num1-num2;
+
+    }
+
+}
